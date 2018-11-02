06692b30ce0b3d359d62c4525167d3b938cba766
veerat-beri <veerat@primedigital.tech>
2018-11-02 11:19:28 +0530

    wip

--- tree/build_tree.py
+++ tree/build_tree.py
@@ -1,5 +1,5 @@
 from tree.tree import LinkedBinaryTree
-
+from collections import deque
 
 class BuildLinkedBinaryTree:
     def __init(self, tree: LinkedBinaryTree=None, list_of_nodes: [LinkedBinaryTree.BinaryTreeNode]=[], auto_populate=False, **kwargs):
@@ -21,10 +21,17 @@ class BuildLinkedBinaryTree:
         if self.list_of_nodes:
             root_node = self._tree.add_root(self.list_of_nodes[0])
             index = 1
+            bfs_queue = deque()
+            bfs_queue.append(root_node)
             while index <= len(self.list_of_nodes) - 1:
+                root_node = bfs_queue.popleft()
                 root_node.add_left_child(self.list_of_nodes[index])
                 index += 1
-                root_node.add_left_child(node_data)
+                bfs_queue.append(self._tree.left(root_node))
+                root_node.add_right_child(self.list_of_nodes[index])
+                index += 1
+                bfs_queue.append(self._tree.left(root_node))
+
 
 
         return self._tree
