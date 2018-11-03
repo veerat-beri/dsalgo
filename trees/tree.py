9bc5670531ffa2f49b2b44b886fd64ebf7ff4ec6
veerat-beri <veerat@primedigital.tech>
2018-11-03 19:16:41 +0530

    wip

--- trees/tree.py
+++ trees/tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from trees.decorators import set_default_node
 
 
@@ -57,15 +59,7 @@ class Tree:
 
 
 class BinaryTreeTraversalMixin:
-    @set_default_node
-    def pre_order(self, node = None):
-        # if node is None:
-        #     node = self.root()
-        if node is not None:
-            for node in self._subtree_preorder(node):
-                yield node
-
-    def _subtree_preorder(self, node: BinaryTreeNode):
+    def _subtree_preorder(self, node):
         ###############
         # 1st Approach
         yield node
@@ -82,19 +76,26 @@ class BinaryTreeTraversalMixin:
         # self._subtree_preorder(self.right(node))
         ###############
 
-    @set_default_node
-    def in_order(self, node: BinaryTreeNode = None):
-        if node is not None:
-            for node in self._subtree_inorder(node):
-                yield node
-
-    def _subtree_inorder(self, node: BinaryTreeNode):
+    def _subtree_postorder(self, node):
         ###############
         # 1st Approach
-        # for child in self.children(node):
-        #     for other_node in self._subtree_preorder(child):
-        #         yield other_node
+        for child in self.children(node):
+            for other_node in self._subtree_preorder(child):
+                yield other_node
+        yield node
+        ###############
+        # 2nd Approach
+        # if node is None:
+        #     return None
+        #
+        # self._subtree_postorder(self.left(node))
+        # self._subtree_postorder(self.right(node))
         # yield node
+        ###############
+
+    def _subtree_inorder(self, node):
+        ###############
+        # 1st Approach
         if self.left(node):
             for other_node in self._subtree_preorder(self.left(node)):
                 yield other_node
@@ -107,33 +108,52 @@ class BinaryTreeTraversalMixin:
         # if node is None:
         #     return None
         #
-        # self._subtree_preorder(self.left(node))
+        # self._subtree_inorder(self.left(node))
         # yield node
-        # self._subtree_preorder(self.right(node))
+        # self._subtree_inorder(self.right(node))
         ###############
 
     @set_default_node
-    def post_order(self, node: BinaryTreeNode = None):
+    def pre_order(self, node=None):
+        # if node is None:
+        #     node = self.root()
+        if node is not None:
+            for node in self._subtree_preorder(node):
+                yield node
+
+    @set_default_node
+    def in_order(self, node=None):
+        if node is not None:
+            for node in self._subtree_inorder(node):
+                yield node
+
+    @set_default_node
+    def post_order(self, node=None):
         if node is not None:
             for node in self._subtree_postorder(node):
                 yield node
 
-    def _subtree_postorder(self, node: BinaryTreeNode):
-        ###############
-        # 1st Approach
-        for child in self.children(node):
-            for other_node in self._subtree_preorder(child):
-                yield other_node
-        yield node
-        ###############
-        # 2nd Approach
-        # if node is None:
-        #     return None
-        #
-        # self._subtree_preorder(self.left(node))
-        # self._subtree_preorder(self.right(node))
-        # yield node
-        ###############
+    @set_default_node
+    def bfs(self, node):
+        bfs_queue = deque()
+        bfs_queue.append(node)
+
+        while bfs_queue:
+            node = bfs_queue.popleft()
+            yield node
+            ###############
+            # 1st Approach
+            # left_child = self.left(node)
+            # right_child = self.right(node)
+            # if left_child is not None:
+            #     bfs_queue.append(left_child)
+            # if right_child is not None:
+            #     bfs_queue.append(right_child)
+            ###############
+            # 2nd Approach
+            for child in self.children(node):
+                bfs_queue.append(child)
+            ###############
 
 
 class BinaryTree(BinaryTreeTraversalMixin, Tree):
