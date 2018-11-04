8b11b2b7e4779434eecdc74445282e92037e6490
veerat-beri <veerat@primedigital.tech>
2018-11-04 13:11:12 +0530

    mostly stable

--- practical_questions/LRU_cache.py
+++ practical_questions/LRU_cache.py
@@ -1,32 +1,25 @@
-from linkedlists.linked_list import BuildSinglyLinkedList, SinglyLinkedListNode
-
-
-
-
-MAX_LINKED_LIST_SIZE = 4
-
-
-class LRUCache:
-    def __init__(self, singly_linked_list_of_records=None, list_of_nodes: [SinglyLinkedListNode]=None):
-        self.singly_linked_list_of_records = BuildSinglyLinkedList(singly_linked_list_of_records, list_of_nodes).build()
-        self.key_node_map = {}
-        self.len_of_linked_list = 0
-
-    def get_attr(self, key):
-        return self.key_node_map
-
-    def set_attr(self, key, value):
-        # if not self.is_linked_list_full:
-        #     self.
-        pass
-
-    @property
-    def is_linked_list_full(self):
-        return self.len_of_linked_list == MAX_LINKED_LIST_SIZE
-
-
-
-
-
-
-# driver code
+# from linkedlists import BuildSinglyLinkedList, SinglyLinkedListNode
+#
+# MAX_LINKED_LIST_SIZE = 4
+#
+#
+# class LRUCache:
+#     def __init__(self, singly_linked_list_of_records=None, list_of_nodes: [SinglyLinkedListNode]=None):
+#         self.singly_linked_list_of_records = BuildSinglyLinkedList(singly_linked_list_of_records, list_of_nodes).build()
+#         self.key_node_map = {}
+#         self.len_of_linked_list = 0
+#
+#     def get_attr(self, key):
+#         return self.key_node_map
+# 
+#     def set_attr(self, key, value):
+#         # if not self.is_linked_list_full:
+#         #     self.
+#         pass
+#
+#     @property
+#     def is_linked_list_full(self):
+#         return self.len_of_linked_list == MAX_LINKED_LIST_SIZE
+#
+#
+# # driver code
