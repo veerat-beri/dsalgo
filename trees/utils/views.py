14c66db9c98ed82bf7b447b245b754732bdbb14a
veerat-beri <veerat@primedigital.tech>
2018-11-04 19:14:06 +0530

    wip

--- trees/utils/views.py
+++ trees/utils/views.py
@@ -1,18 +1,57 @@
+# Problem Statement
+# https://www.geeksforgeeks.org/print-left-view-binary-tree/
+
 from collections import deque
 
 from trees.build_tree import BuildLinkedBinaryTree
 from trees.tree import LinkedBinaryTree, BinaryTree
 
 
-# Problem Statement
-# https://www.geeksforgeeks.org/print-left-view-binary-tree/
+# Standard Solution
+# def left_view(tree: LinkedBinaryTree):
+#     if tree.is_empty():
+#         raise ValueError('Tree is Empty')
+#
+#     ###############
+#     # 1st Approach: BFS
+#     def _left_view(node: BinaryTree.BinaryTreeNode, traversal_level):
+#         bfs_queue = deque()
+#         bfs_queue.append((node, 0))
+#         yield node
+#
+#         while bfs_queue:
+#             node, node_level = bfs_queue.popleft()
+#             if node_level > traversal_level:
+#                 traversal_level = node_level
+#                 yield node
+#             ###############
+#             # 1st Approach
+#             # left_child = self.left(node)
+#             # right_child = self.right(node)
+#             # if left_child is not None:
+#             #     bfs_queue.append((left_child, node_level + 1))
+#             # if right_child is not None:
+#             #     bfs_queue.append((right_child, node_level + 1))
+#             ###############
+#             # 2nd Approach
+#             for child in tree.children(node):
+#                 bfs_queue.append((child, node_level + 1))
+#             ###############
+#
+#     for node in _left_view(tree.root(), 0):
+#         yield node
+#
+#     ###############
+#     # 2nd Approach: DFS
+
+# Full-proof Solution
 def left_view(tree: LinkedBinaryTree):
     if tree.is_empty():
         raise ValueError('Tree is Empty')
 
     ###############
     # 1st Approach: BFS
-    def _left_view(node: BinaryTree.BinaryTreeNode, traversal_level):
+    def _left_view(node: BinaryTree.BinaryTreeNode, traversal_level, horizontal_distance):
         bfs_queue = deque()
         bfs_queue.append((node, 0))
         yield node
@@ -36,15 +75,11 @@ def left_view(tree: LinkedBinaryTree):
                 bfs_queue.append((child, node_level + 1))
             ###############
 
-    for node in _left_view(tree.root(), 0):
+    for node in _left_view(tree.root(), 0, 0):
         yield node
 
     ###############
     # 2nd Approach: DFS
-
-
-def right_view(tree: LinkedBinaryTree):
-    pass
 
 
 # driver code
@@ -54,10 +89,6 @@ def run():
     print('Left-view: ')
     for node in left_view(tree):
         print(tree.element(node), end=' ')
-
-    # print('Right-view: ')
-    # for node in right_view(tree):
-    #     print(tree.element(node), end=' ')
 
 
 if __name__ == '__main__':
