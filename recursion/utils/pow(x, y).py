b3001043c1929c08cb563166bc8ab15a73737f7c
veerat-beri <veerat@primedigital.tech>
2018-11-15 20:29:39 +0530

    wip

--- recursion/utils/pow(x, y).py
+++ recursion/utils/pow(x, y).py
@@ -2,9 +2,24 @@
 # https://www.geeksforgeeks.org/write-a-c-program-to-calculate-powxn/
 
 
-def cal_pow(x, y):
-    if y == 1:
-        return x
+def power(x, y):
+    if y == 0:
+        return 1
 
-    if y%2 == 0:
-        return cal_pow()
+    if y % 2 == 0:
+        return power(x, y // 2) * power(x, y // 2)
+    return x * power(x, y // 2) * power(x, y // 2)
+
+
+# driver code
+def run():
+    x = 10
+    y = 5
+    ans = power(x, y)
+
+    print(f'pow({x}, {y}) is: ', ans)
+
+
+if __name__ == '__main__':
+    run()
+
