d09172b33bf45daf3ff99d7689ffd60006da93a9
veerat-beri <veerat@primedigital.tech>
2018-11-22 11:35:15 +0530

    wip

--- arrays/every_subarray_distinct_count.py
+++ arrays/every_subarray_distinct_count.py
@@ -1,8 +1,19 @@
 # Problem statement
 # https://www.geeksforgeeks.org/count-distinct-elements-in-every-window-of-size-k/
 
+
 def get_every_subarray_distinct_count(arr, subarray_window_size):
     current_subarray_elem_count = {}
-    for index in range(subarray_window_size):
-        
+    if len(arr) < subarray_window_size:
+        raise Exception('Array size is less than window size!')
 
+    current_subarray_distinct_count = 0
+    for index in range(subarray_window_size):
+        if current_subarray_elem_count.get(arr[index]):
+            current_subarray_elem_count[arr[index]] += 1
+        else:
+            current_subarray_elem_count[arr[index]] = 1
+            current_subarray_distinct_count += 1
+
+
+        
