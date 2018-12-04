2924b880b8986756f87437ed9f9398a8f0c43708
veerat-beri <veerat@primedigital.tech>
2018-12-04 10:24:22 +0530

    wip

--- linkedlists/utils/remove_duplicates_from_unsorted_list.py
+++ linkedlists/utils/remove_duplicates_from_unsorted_list.py
@@ -1,3 +1,28 @@
 # Problem Statement
 # https://www.geeksforgeeks.org/remove-duplicates-from-an-unsorted-linked-list/
+from linkedlists import SinglyLinkedList, BuildSinglyLinkedList, SinglyLinkedListNode
+
+
+def remove_duplicates_from_unsorted_list(linked_list: SinglyLinkedList):
+    pass
+
+
+# driver code
+def run():
+    list_of_nodes = [
+        SinglyLinkedListNode(10), SinglyLinkedListNode(10), SinglyLinkedListNode(20),
+        SinglyLinkedListNode(30), SinglyLinkedListNode(30), SinglyLinkedListNode(30)
+    ]
+    sorted_singly_singly_list = BuildSinglyLinkedList(list_of_nodes=list_of_nodes).build()
+
+    print('Given Lined List: ')
+    sorted_singly_singly_list.print_linked_list()
+
+    print('\n\nLinked List after duplicate removal: ')
+    remove_duplicates_from_unsorted_list(sorted_singly_singly_list)
+    sorted_singly_singly_list.print_linked_list()
+
+
+if __name__ == '__main__':
+    run()
 
