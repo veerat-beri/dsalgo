ac3e02936df82b857c3f763b41f92e3e907d2487
veerat-beri <veerat@primedigital.tech>
2018-12-11 23:32:07 +0530

    wip

--- DP/utils/longest_pallindromic_subsequence.py
+++ DP/utils/longest_pallindromic_subsequence.py
@@ -2,26 +2,28 @@
 # https://www.geeksforgeeks.org/longest-palindromic-subsequence-dp-12/
 
 
-def get_longest_palindromic_subsequence(input_str):
-    def _get_longest_palindromic_subsequence(start_index, end_index):
-        if start_index >= end_index:
-            return 0
-        if input_str[start_index] == input_str[end_index]:
-            return 1 + _get_longest_palindromic_subsequence(start_index + 1, end_index - 1)
-        return max(_get_longest_palindromic_subsequence(start_index + 1, end_index),
-                   _get_longest_palindromic_subsequence(start_index, end_index - 1)) + 1
+def get_longest_palindromic_subsequence(input_str: str):
+    def _get_longest_palindromic_subsequence(start_index: int, end_index: int):
+        if start_index == end_index:
+            return 1
+        if input_str[start_index] == input_str[end_index] and start_index + 1 == end_index:
+            return 2
 
-    return _get_longest_palindromic_subsequence(input_str[0], input_str[-1])
+        if input_str[start_index] == input_str[end_index]:
+            return 2 + _get_longest_palindromic_subsequence(start_index + 1, end_index - 1)
+        return max(_get_longest_palindromic_subsequence(start_index + 1, end_index),
+                   _get_longest_palindromic_subsequence(start_index, end_index - 1))
+
+    return _get_longest_palindromic_subsequence(0, len(input_str) - 1)
 
 
 # driver code
 def run():
     input_str = "geeksforgeeks"
+    # input_str = "ee"
     print('Longest Palindromic Subsequence length is: ', end='')
     print(get_longest_palindromic_subsequence(input_str))
 
 
-if __name__ == ''
-
-
-
+if __name__ == '__main__':
+    run()
