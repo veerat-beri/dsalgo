2c213970cf67fe7929a9d8b9025cb579bbdfcfbb
veerat-beri <veerat@primedigital.tech>
2018-12-29 17:42:54 +0530

    wip

--- arrays/utils/sub_array_with_zero_sum.py
+++ arrays/utils/sub_array_with_zero_sum.py
@@ -22,25 +22,30 @@ def check_zero_sum_subarray_exists(arr):
 def get_zero_sum_subarray(arr):
     indexed_prefix_sum = {}
     arr_sum_till_point = 0
-    max_length_zero_sum_subarray = ()
+    max_length_zero_sum_subarray_indexes = ()
+    zero_sum_subarray_max_length = 0
     for index in range(len(arr)):
         arr_sum_till_point += arr[index]
-        if arr_sum_till_point in indexed_prefix_sum:
-            zero_subarray_indexes = indexed_prefix_sum[arr_sum_till_point]
-            zero_subarray_indexes.append(index)
-            if not max_length_zero_sum_subarray or (max_length_zero_sum_subarray[1] - max_length_zero_sum_subarray[0] <
-                                                    zero_subarray_indexes[-1] - zero_subarray_indexes[0]):
-                max_length_zero_sum_subarray = (zero_subarray_indexes[0], zero_subarray_indexes[-1])
+        if arr_sum_till_point == 0:
+            zero_sum_subarray_max_length = index + 1
+            max_length_zero_sum_subarray_indexes = (0, index)
 
-        indexed_prefix_sum[arr_sum_till_point] = [index]
-    return max_length_zero_sum_subarray
+        if arr_sum_till_point in indexed_prefix_sum:
+            if zero_sum_subarray_max_length < index - indexed_prefix_sum[arr_sum_till_point]:
+                zero_sum_subarray_max_length = index - indexed_prefix_sum[arr_sum_till_point]
+                max_length_zero_sum_subarray_indexes = (indexed_prefix_sum[arr_sum_till_point] + 1, index)
+
+        elif arr_sum_till_point != 0:
+            indexed_prefix_sum[arr_sum_till_point] = index
+    return zero_sum_subarray_max_length, max_length_zero_sum_subarray_indexes
 
 
 # driver code
 def run():
-    # arr = [-2, -3, 4, -1, -2, 1, 5, -3]
-    # arr = [4, -2, -1, -1, -1]
-    arr = [15, -2, 2, -1, 1, 20, 10, -5, -3, -1, -1, 0, ]
+    # arr = [1, 1, 0, 0, ]
+    # arr = [4, -2, -1, -1, -1, 1]
+    arr = [15, -2, 2, -1, 1, 10, -5, -3, -1, -1, 0, ]
+    # arr = [15, -2, 2, -1, 1, 20, 10, -5, -3, -1, -1, 0, ]
 
     ###############
     # Check whether zero sum sub-array exists or not
@@ -50,9 +55,9 @@ def run():
     # print(message)
     ###############
 
-    subarray_indexes = get_zero_sum_subarray(arr)
-    if subarray_indexes:
-        print(f'Max Length({subarray_indexes[1] - subarray_indexes[0]}) Sub-array with zero sum: ', arr[subarray_indexes[0] + 1: subarray_indexes[1] + 1], sep='\n')
+    subarray_length, subarray_indexes = get_zero_sum_subarray(arr)
+    if subarray_length:
+        print(f'Max Length({subarray_length}) Sub-array with zero sum: ', arr[subarray_indexes[0]: subarray_indexes[1] + 1], sep='\n')
     else:
         print('Sub-array with zero sum do not exists!')
 
