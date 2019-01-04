29ee83e68547e0a7ce4f0e1d8d75e188072688cc
veerat-beri <veerat@primedigital.tech>
2019-01-04 22:07:29 +0530

    wip

---
+++ mathematical/bit_manipulations/highest_power_of_2.py
@@ -0,0 +1,22 @@
+# Problem Statement
+# https://www.geeksforgeeks.org/highest-power-2-less-equal-given-number/
+import sys
+
+
+def get_max_power_of_2(num: int):
+    dup_num = num
+    count = 0
+    while dup_num:
+        dup_num = dup_num // 2
+        count += 1
+    return count
+
+
+# driver code
+def run():
+    num = sys.maxsize
+    print(f'Max power of 2 in {num}: ', get_max_power_of_2(num), sep=' ')
+
+
+if __name__ == '__main__':
+    run()
