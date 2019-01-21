499a233770e432ab8c69af04c606b331796800a4
veerat-beri <veerat@primedigital.tech>
2019-01-21 21:41:49 +0530

    wip

--- linkedlists/utils/__init__.py
+++ linkedlists/utils/__init__.py
@@ -1,8 +1,9 @@
+
 __all__ = [
     'check_loop_exists', 'find_mid_of_linked_list', 'reverse_linked_list',
     'delete_n_nodes_after_every_m_nodes', 'reverse_in_group_of_k', 'remove_duplicates_from_sorted_list',
     'find_kth_node_from_end', 'point_of_intersection_of_two_lists', 'remove_duplicates_from_unsorted_list',
-    'remove_loop',
+    'remove_loop', 'get_sorted_merged_ll',
 ]
 
 from .detect_loop import check_loop_exists
@@ -15,3 +16,4 @@ from .kth_elem_from_last import find_kth_node_from_end
 from .point_of_intersection_of_two_lists import point_of_intersection_of_two_lists
 from .remove_duplicates_from_unsorted_list import remove_duplicates_from_unsorted_list
 from .remove_loop import remove_loop
+from .merge_two_sorted_ll import get_sorted_merged_ll
--- linkedlists/utils/merge_sort.py
+++ linkedlists/utils/merge_sort.py
@@ -2,24 +2,24 @@
 # https://www.geeksforgeeks.org/merge-sort-for-linked-list/
 
 
-from linkedlists import BuildSinglyLinkedList, SinglyLinkedList
-
-
-def get_merge_sorted_ll(linked_list: SinglyLinkedList):
-    def _get_merge_sorted_ll():
-        pass
-
-
-# driver code
-def run():
-    list_of_nodes = [12, 11, 13, 5, 6, 7, ]
-    singly_linked_list = BuildSinglyLinkedList(list_of_nodes=list_of_nodes).get_ll()
-    print('Linked List before sort: ')
-    singly_linked_list.print_linked_list()
-
-    print('\nLinked List after sort: ')
-    singly_linked_list.print_linked_list()
-
-
-if __name__ == '__main__':
-    run()
+# from linkedlists import BuildSinglyLinkedList, SinglyLinkedList
+#
+#
+# def get_merge_sorted_ll(linked_list: SinglyLinkedList):
+#     def _get_merge_sorted_ll():
+#         pass
+#
+#
+# # driver code
+# def run():
+#     list_of_nodes = [12, 11, 13, 5, 6, 7, ]
+#     singly_linked_list = BuildSinglyLinkedList(list_of_nodes=list_of_nodes).get_ll()
+#     print('Linked List before sort: ')
+#     singly_linked_list.print_linked_list()
+#
+#     print('\nLinked List after sort: ')
+#     singly_linked_list.print_linked_list()
+#
+#
+# if __name__ == '__main__':
+#     run()
