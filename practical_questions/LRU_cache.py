73831d942f8cb3d747e6aad14b84f9e7553c7ff5
veerat-beri <veerat@primedigital.tech>
2019-02-09 21:33:10 +0530

    wip

--- practical_questions/LRU_cache.py
+++ practical_questions/LRU_cache.py
@@ -6,13 +6,12 @@ from linkedlists import BuildDoublyLinkedList, DoublyLinkedList
 
 MAX_CACHE_SIZE = 4
 
+
 class LRUCache:
-    def __init__(self):
+    def __init__(self, cache_size=MAX_CACHE_SIZE):
         self.deque = self.get_deque_instance()
         self.key_node_map = {}
-
-    def get_attr(self, key):
-        return self.key_node_map[key]
+        self.max_cache_size = cache_size
 
     def get_new_node(self, node_data):
         return self.deque._get_new_node(node_data)
@@ -28,10 +27,14 @@ class LRUCache:
         else:
             node.next.previous = node.previous
 
-    def remove(self, attr_key=None, node=None):
-        del self.key_node_map[attr_key or self.deque.tail.data]
-        node or self.deque.tail
-        self._remove_deque_node(node or self.deque.tail)
+    def remove(self, node):
+        del self.key_node_map[node.data]
+        self._remove_deque_node(node)
+
+    def pop_right(self):
+        tail_node = self.deque.tail
+        self.remove(tail_node)
+        return tail_node
 
     def append_left(self, new_deque_node):
         new_deque_node.next = self.deque.head
@@ -39,31 +42,40 @@ class LRUCache:
         self.deque.head = new_deque_node
         new_deque_node.previous = None
 
-    def set_attr(self, attr_key, attr_value):
-        ll_node = self.key_node_map.get(attr_key, None)
-        if ll_node:
-            self.remove(attr_key, ll_node)
-            self.append_left(ll_node)
+    def get_attr(self, attr_key):
+        deque_node = self.key_node_map.get(attr_key, None)
+        if deque_node:
+            self.remove(deque_node)
+            self.append_left(deque_node)
         else:
             if self.is_deque_full:
-                self.remove()
-                self.append_left(self.get_new_node(attr_value))
+                tail_node = self.pop_right()
+                tail_node.data = attr_key
+                self.append_left(tail_node)
+                self.key_node_map[attr_key] = tail_node
             else:
-                self.append_left()
+                new_node = self.get_new_node(attr_key)
+                self.append_left(new_node)
+                self.key_node_map[attr_key] = new_node
 
+    def print_cache(self):
+        for key,  in
 
-    def get_deque_instance(self):
+    @classmethod
+    def get_deque_instance(cls):
         return BuildDoublyLinkedList().get_ll()
 
     @property
     def is_deque_full(self):
         # return self.len_of_linked_list == MAX_CACHE_SIZE
-        return len(self.key_node_map) >= MAX_CACHE_SIZE
+        return len(self.key_node_map) >= self.max_cache_size
 
 
 # driver code
 def run():
-    pass
+    lru_cache = LRUCache()
+    for num in range(1, 6):
+        lru_cache.get_attr(1)
 
 
 if __name__ == '__main__':
