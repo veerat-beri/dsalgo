58b80d26483d6a9152659d69c1a9cab802e22fbd
veerat-beri <veerat@primedigital.tech>
2019-02-13 22:06:06 +0530

    wip

--- trees/utils/construct_tree.py
+++ trees/utils/construct_tree.py
@@ -1,27 +1,47 @@
+from trees import LinkedBinaryTree
+from trees.utils import print_bfs
+
+###########################################################################
 # Problem Statement
 # https://www.geeksforgeeks.org/construct-tree-from-given-inorder-and-preorder-traversal/
 
 
-from trees import LinkedBinaryTree, BuildLinkedBinaryTree
-from trees.utils import print_bfs
-
-
-def get_tree_from_inorder_and_preorder(inorder_path: [], preorder_path: [])-> LinkedBinaryTree:
+def get_bt_from_inorder_and_preorder(inorder_path: [], preorder_path: [])-> LinkedBinaryTree:
     preorder_traversal_index = 0
 
-    def _get_tree_from_inorder_and_preorder(inorder_path):
+    def _get_bt_root_from_inorder_and_preorder(inorder_path):
         nonlocal preorder_traversal_index
+
         if inorder_path:
             inorder_path_index = inorder_path.index(preorder_path[preorder_traversal_index])
             preorder_traversal_index += 1
 
             node = LinkedBinaryTree.get_new_node(inorder_path[inorder_path_index])
-            node._left = _get_tree_from_inorder_and_preorder(inorder_path[:inorder_path_index])
-            node._right = _get_tree_from_inorder_and_preorder(inorder_path[inorder_path_index + 1:])
+            node._left = _get_bt_root_from_inorder_and_preorder(inorder_path[:inorder_path_index])
+            node._right = _get_bt_root_from_inorder_and_preorder(inorder_path[inorder_path_index + 1:])
             return node
 
-    root_node = _get_tree_from_inorder_and_preorder(inorder_path)
+    root_node = _get_bt_root_from_inorder_and_preorder(inorder_path)
     return LinkedBinaryTree(root=root_node)
+
+###########################################################################
+
+# Problem Statement
+# https://www.geeksforgeeks.org/full-and-complete-binary-tree-from-given-preorder-and-postorder-traversals/
+
+
+def get_full_bt_from_preorder_and_postorder(preorder_path: [], postorder_path: [])-> LinkedBinaryTree:
+    preorder_traversal_index = 0
+
+    def _get_bt_root_from_preorder_and_postorder(postorder_path):
+        nonlocal preorder_traversal_index
+        if postorder_path:
+            postorder_path_index = postorder_path.index(preorder_path[preorder_traversal_index + 1])
+
+    root_node = _get_bt_root_from_preorder_and_postorder()
+    return LinkedBinaryTree(root=root_node)
+
+###########################################################################
 
 
 # driver code
@@ -29,7 +49,7 @@ def run():
     inorder_path = ['D', 'B', 'E', 'A', 'F', 'C', ]
     preorder_path = ['A', 'B', 'D', 'E', 'C', 'F', ]
 
-    binary_tree = get_tree_from_inorder_and_preorder(inorder_path, preorder_path)
+    binary_tree = get_bt_from_inorder_and_preorder(inorder_path, preorder_path)
 
     print('Preorder of new Tree constructed is:')
     for node in binary_tree.pre_order():
