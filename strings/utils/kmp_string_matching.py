d5b717c21078d15d528a190da4dcbd17bff767d1
veerat-beri <veerat@primedigital.tech>
2019-02-18 21:46:57 +0530

    Wip

--- strings/utils/kmp_string_matching.py
+++ strings/utils/kmp_string_matching.py
@@ -3,8 +3,18 @@
 
 
 def get_lps_arr(pattern_string: str):
-    lps = []
+    lps = [0]*len(pattern_string)
+    local_max_prefix_len = 0
+    for str_index in range(1, len(pattern_string)):
+        if pattern_string[str_index] == pattern_string[local_max_prefix_len]:
+            local_max_prefix_len += 1
+        else:
+            while local_max_prefix_len > 0 and pattern_string[str_index] == pattern_string[local_max_prefix_len]:
+                local_max_prefix_len = lps[local_max_prefix_len - 1]
+
+        lps[str_index] = local_max_prefix_len
     return lps
+
 
 def get_all_pattern_occurances():
     pass
