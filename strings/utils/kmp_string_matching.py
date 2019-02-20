5a63e037297afca13075309b7b8376321de9f004
veerat-beri <veerat@primedigital.tech>
2019-02-20 11:10:34 +0530

    wip

--- strings/utils/kmp_string_matching.py
+++ strings/utils/kmp_string_matching.py
@@ -5,16 +5,36 @@
 def get_lps_arr(pattern_string: str):
     lps = [0]*len(pattern_string)
     local_max_prefix_len = 0
-    for str_index in range(1, len(pattern_string)):
+    str_index = 1
+    # for str_index in range(1, len(pattern_string)):
+    while str_index < len(pattern_string):
         if pattern_string[str_index] == pattern_string[local_max_prefix_len]:
             local_max_prefix_len += 1
+            lps[str_index] = local_max_prefix_len
+            str_index += 1
         else:
-            while local_max_prefix_len > 0 and pattern_string[str_index] == pattern_string[local_max_prefix_len]:
+            if local_max_prefix_len > 0:
                 local_max_prefix_len = lps[local_max_prefix_len - 1]
+            else:
+                lps[str_index] = 0
+                str_index += 1
 
-        lps[str_index] = local_max_prefix_len
     return lps
 
 
-def get_all_pattern_occurances():
-    pass
+def get_all_pattern_occurrences(pattern_str: str, search_str: str):
+    lps_arr = get_lps_arr(pattern_str)
+
+
+    print(lps_arr)
+
+
+# driver code
+def run():
+    pattern_str = 'AAACAAAA'
+    search_str = ''
+    get_all_pattern_occurrences(pattern_str, search_str)
+
+
+if __name__ == '__main__':
+    run()
---
+++ trees/tries/trie.py
@@ -0,0 +1,14 @@
+from trees.tree import BinaryTree
+
+
+class Trie(BinaryTree):
+    # for only small english chars
+    DEFAULT_TRIE_NODE_CHILDREN_COUNT = 26
+
+    class TrieNode(BinaryTree.BinaryTreeNode):
+        def __init(self, **kwargs):
+            self.children_count = self.TRIE_NODE_CHILDREN_COUNT
+            child_nodes = [] * self.children_count
+
+    def __init__(self, node_children_count: int = None, **kwargs):
+        self.TRIE_NODE_CHILDREN_COUNT = node_children_count or self.DEFAULT_TRIE_NODE_CHILDREN_COUNT
