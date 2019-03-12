a0c49a34481518b2b53111e7c90cbbedd57450af
veerat-beri <veerat@primedigital.tech>
2019-03-12 10:53:58 +0530

    Wip

--- heaps/utils/check_array_repr_heap.py
+++ heaps/utils/check_array_repr_heap.py
@@ -1,6 +1,33 @@
 # Problem Statement
 # https://www.geeksforgeeks.org/how-to-check-if-a-given-array-represents-a-binary-heap/
+import heapq
 
 
-def do_arr_repr_heap(heap_type='MAX', ):
-    pass
+def do_arr_repr_heap(heap: [], heap_type='MAX', ):
+    heap_len = len(heap)
+    index = 0
+    for index in range(heap_len - heap_len//2):
+        try:
+            if heap[index] > heap[2*index + 1] and heap[index] > heap[2*index + 2]:
+                continue
+        except IndexError:
+            pass
+        break
+    if index >= heap_len - heap_len//2 - 1:
+        return True
+    return False
+
+
+# driver code
+def run():
+    heap = [90, 15, 10, 7, 12, 2]
+    heapq.heapify(heap)
+    print(f'Given arr: {heap}')
+    if do_arr_repr_heap(heap):
+        print('Represents Max Heap')
+    else:
+        print('It does not represent Max Heap')
+
+
+if __name__ == '__main__':
+    run()
