26d13e928df9a2ba1b618d137fbf6c59ed75df6c
veerat-beri <veerat@primedigital.tech>
2019-03-15 21:41:05 +0530

    Wip

--- arrays/utils/triplet_with_given_sum.py
+++ arrays/utils/triplet_with_given_sum.py
@@ -2,7 +2,7 @@
 # https://www.geeksforgeeks.org/find-a-triplet-that-sum-to-a-given-value/
 
 
-def check_triplet_exists(arr: [], triplet_sum):
+def get_triplet_of_sum(arr: [], triplet_sum):
     # Hash Based Approach
     # Time Complexity: O(n^2)
     # Space Complexity: O(n)
@@ -14,7 +14,7 @@ def check_triplet_exists(arr: [], triplet_sum):
         for sub_array_index in range(arr_index + 1, arr_len):
             another_elem_in_triplet = arr[sub_array_index]
             if required_pair_sum - another_elem_in_triplet in arr_elem_so_far:
-                return arr[arr_index], required_pair_sum - another_elem_in_triplet, required_pair_sum - another_elem_in_triplet
+                return arr[arr_index], required_pair_sum - another_elem_in_triplet, another_elem_in_triplet
             arr_elem_so_far.add(another_elem_in_triplet)
 
 
@@ -22,10 +22,13 @@ def check_triplet_exists(arr: [], triplet_sum):
 def run():
     arr = [1, 4, 45, 6, 10, 8]
     triplet_sum = 22
-    if check_triplet_exists(arr, triplet_sum):
-        print(f'Yes Triplet with sum: {triplet_sum} exists')
+    triplet_nums = get_triplet_of_sum(arr, triplet_sum)
+
+    print(f'Given arr: {arr}')
+    if triplet_nums:
+        print(f'Yes Triplet with sum({triplet_sum}) exists: {triplet_nums[0]}, {triplet_nums[1]}, {triplet_nums[2]}')
         return
-    print(f'No Triplet with sum: {triplet_sum} does not exists')
+    print(f'No Triplet with sum({triplet_sum}) does not exists')
 
 
 if __name__ == '__main__':
