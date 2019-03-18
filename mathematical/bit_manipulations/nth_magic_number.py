58812030063f35d9888075d3233c3dc34355239d
veerat-beri <veerat@primedigital.tech>
2019-03-18 21:55:20 +0530

    wip

---
+++ mathematical/bit_manipulations/nth_magic_number.py
@@ -0,0 +1,15 @@
+# Problem Statement
+# https://www.geeksforgeeks.org/find-nth-magic-number/
+
+
+def get_nth_magic_num(n: int):
+    int()
+
+
+# driver code
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
