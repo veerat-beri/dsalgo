2df73d6f742711b6ebbfcff457138dd30dc98a45
veerat-beri <veerat@primedigital.tech>
2019-03-26 21:53:33 +0530

    wip

--- arrays/utils/rotate_arr.py
+++ arrays/utils/rotate_arr.py
@@ -2,23 +2,30 @@
 # https://www.geeksforgeeks.org/array-rotation/
 
 
-# Efficient Juggling Algorithm
 from mathematical.school_problems import get_gcd
 
 
+# Efficient Juggling Algorithm
+# Time Complexity: O(N) OR O( gcd(n, d)*(n//d + 1) )
+# Space Complexity: O(1)
 def get_rotated_arr(arr: [], total_rotations: int):
     assert arr, 'Array could not be empty'
 
     arr_len = len(arr)
     for rotation_set_index in range(get_gcd(arr_len, total_rotations)):
         set_first_rotated_elem = arr[rotation_set_index]
-        first_rotated_index = rotation_set_index
+        previous_rotated_index = rotation_set_index
         while 1:
-            next_index = total_rotations + first_rotated_index
-            if next_index == arr_len:
+            next_rotating_index = total_rotations + previous_rotated_index
+            if next_rotating_index >= arr_len:
+                next_rotating_index = next_rotating_index - arr_len
+            if next_rotating_index == rotation_set_index:
                 break
-            arr[next_index] = arr[next_index + total_rotations]
-        arr[next_index - 1] = set_first_rotated_elem
+
+            arr[previous_rotated_index] = arr[next_rotating_index]
+            previous_rotated_index = next_rotating_index
+
+        arr[previous_rotated_index] = set_first_rotated_elem
     return arr
 
 
