7b7dce129c16bd5068de6501c54d00c015eb2335
veerat-beri <veerat@primedigital.tech>
2019-04-02 21:48:26 +0530

    wip

--- arrays/utils/kth_smallest_in_unsorted_arr.py
+++ arrays/utils/kth_smallest_in_unsorted_arr.py
@@ -1,38 +1,54 @@
 # Problem Statement
 # https://www.geeksforgeeks.org/kth-smallestlargest-element-unsorted-array/
-import heapq
 
 
-def get_kth_smallest_num(arr: [], k):
+from heaps.heap import MaxHeap, MinHeap
+
+
+def get_kth_smallest_num(arr: [], k, use_min_heap=False):
     # Using Min Heap
     # Time-complexity: O(N + k*log k)
-    heap = arr
     arr_len = len(arr)
     assert k <= arr_len, 'k can not be greater than arr-len'
 
     def _use_min_heap():
-        heapq.heapify(heap)
+        print('Using MIN-Heap..')
+
+        heap = MinHeap(arr)
+
         for _ in range(k - 1):
-            heapq.heappop(heap)
+            heap.pop()
 
-        return heapq.heappop(heap)
+        return heap.pop()
 
-    return _use_min_heap()
-
-    # Using Max Heap (Optimised)
+    # Using Max Heap (OPTIMISED)
     # Time-complexity: O(k + (N-k)*log k)
     def _use_max_heap():
-        heapq._heapify_max(heap)
+        print('Using MAX-Heap..')
 
-        for elem in range(k, arr_len):
-            if elem in ran
+        heap = MaxHeap(arr[:k])
+
+        for arr_index in range(k, arr_len):
+            elem = arr[arr_index]
+            if elem < heap.get_max():
+                heap.replace_root(elem)
+
+        return heap.pop()
+
+    # Using Quick Sort
+    def _use_quick_sort():
+        pass
+
+    executing_func = _use_min_heap if use_min_heap is True else _use_max_heap
+    return executing_func()
+
 
 # driver code
 def run():
     arr = [12, 3, 5, 7, 19, ]
     k = 2
     print(f'Given array: {arr} ')
-    print(f'Kth({k}) smallest num in given arr is: {get_kth_smallest_num(arr, k)}')
+    print(f'Kth({k}) smallest num in given arr is: {get_kth_smallest_num(arr, k, use_min_heap=False)}')
 
 
 if __name__ == '__main__':
--- heaps/heap.py
+++ heaps/heap.py
@@ -9,54 +9,73 @@ class _Heap:
             super().__init__(data, left, right, **kwargs)
 
     def __init__(self, arr: []):
-        self._size = 0
-        self.arr = arr
-        self.heapify(self.arr)
+        self._size = len(arr)
+        self._arr = arr or []
+        self.heap = self._arr
+        self.heapify()
 
     def __len__(self):
         return self._size
 
+    def __str__(self):
+        return self.heap
+
+    def __repr__(self):
+        return str(self.heap)
+
     def heapify(self):
+        raise NotImplementedError('Has to be Implemented by sub class')
+
+    def replace_root(self, elem: int):
         raise NotImplementedError('Has to be Implemented by sub class')
 
     def is_empty(self):
         return bool(self._size)
 
+    def pop(self):
+        self._size -= 1
+        return heapq.heappop(self.heap)
+
     def get_parent(self, child_index):
-        return (child_index - 1) // 2
+        parent_index = (child_index - 1) // 2
+        assert parent_index >= 0, 'No parent exists'
+        return parent_index
 
     def get_left_child_index(self, parent_index):
         try:
-            return self.arr[parent_index*2 + 1]
+            return self.heap[parent_index*2 + 1]
         except IndexError:
             print('Left Child don\'t exists')
 
     def get_right_child_index(self, parent_index):
         try:
-            return self.arr[parent_index*2 + 2]
+            return self.heap[parent_index*2 + 2]
         except IndexError:
             print('Right Child don\'t exists')
 
 
 class MinHeap(_Heap):
     def get_min(self):
-        assert self.arr, 'Heap cannot be empty'
-        return self.arr[0]
+        assert self.heap, 'Heap cannot be empty'
+        return self.heap[0]
 
     def heapify(self):
-        return heapq.heapify(self.arr)
+        return heapq.heapify(self.heap)
 
-    def heappop(self):
-        return heapq.heappop(self.arr)
+    def replace_root(self, elem: int):
+        heapq.heapreplace(self.heap, elem)
 
 
 class MaxHeap(_Heap):
     def get_max(self):
-        assert self.arr, 'Heap cannot be empty'
-        return self.arr[0]
+        assert self.heap, 'Heap cannot be empty'
+        return self.heap[0]
 
     def heapify(self):
-        return heapq._heapify_max(self.arr)
+        return heapq._heapify_max(self.heap)
 
-    def heappop(self):
-        return heapq
+    def replace_root(self, elem: int):
+        heapq._heapreplace_max(self.heap, elem)
+
+    # def heappop(self):
+    #     return heapq
