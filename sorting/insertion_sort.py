cdce4f6f9ee0693d20a1816a74833ce26b48f2a9
veerat-beri <veerat@primedigital.tech>
2019-04-07 10:26:38 +0530

    wip

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -8,13 +8,20 @@ def get_sorted_arr(arr: []):
     arr_len = len(arr)
 
     for arr_index in range(1, arr_len):
-        test_elem_index = arr_index
-
-
-        print(test_elem_index)
-        while test_elem_index and arr[test_elem_index - 1] < arr[test_elem_index]:
-            swap_arr_elem(arr_index, test_elem_index, arr)
+        # More Swaps
+        # test_elem_index = arr_index
+        # while test_elem_index and arr[test_elem_index - 1] > arr[test_elem_index]:
+        #     swap_arr_elem(test_elem_index - 1, test_elem_index, arr)
+        #     test_elem_index -= 1
+        ##############################
+        # Less Swaps
+        comparator_elem = arr[arr_index]
+        test_elem_index = arr_index - 1
+        while test_elem_index >= 0 and arr[test_elem_index] > comparator_elem:
+            arr[test_elem_index + 1] = arr[test_elem_index]
             test_elem_index -= 1
+        arr[test_elem_index + 1] = comparator_elem
+        ##############################
 
 
 # driver code
