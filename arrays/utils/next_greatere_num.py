0e399c26c73a1d81990857da2e2a8b640d40f764
veerat-beri <veerat@primedigital.tech>
2019-04-12 10:59:17 +0530

    wip

--- arrays/utils/next_greatere_num.py
+++ arrays/utils/next_greatere_num.py
@@ -5,9 +5,11 @@
 from arrays.services import swap_arr_elem
 
 
-def get_next_greater_num(num: []):
+def get_next_greater_num(input_num: [], use_time_optimised=False):
+    num = input_num[:]
     num_len = len(num)
 
+    ############################################################
     # Time complexity: O(N * log2 N + 2N)
     # Space Complexity: O(1)
     def _get_swapping_indexes():
@@ -30,30 +32,34 @@ def get_next_greater_num(num: []):
 
         return pivot_index, swapping_index
 
+    ############################################################
     # Time complexity: O(N * log2 N + N)
     # Space Complexity: O(N)
+    def _get_min(num: [], arr_index: int, min_so_far_list: []):
+        if min_so_far_list[arr_index + 1][0] > num[arr_index]:
+            return num[arr_index], arr_index
+        return min_so_far_list[arr_index + 1]
+
     def _get_swapping_indexes_TO():
         pivot_index = -1
-        min_so_far_list = []
+        min_so_far_list = [0] * num_len
+        min_so_far_list[-1] = (num[-1], num_len - 1)
+
         for arr_index in range(num_len - 2, -1, -1):
             if num[arr_index] < num[arr_index + 1]:
                 pivot_index = arr_index
                 break
+            min_so_far_list[arr_index] = _get_min(num, arr_index, min_so_far_list)
 
         if pivot_index == -1:
             return None, None
 
-        swapping_index = pivot_index + 1
-        next_greater_digit = num[swapping_index]
+        return pivot_index, min_so_far_list[pivot_index + 1][1]
 
-        for arr_index in range(pivot_index + 2, num_len):
-            if num[arr_index] < next_greater_digit:
-                next_greater_digit = num[arr_index]
-                swapping_index = arr_index
+    ############################################################
 
-        return pivot_index, swapping_index
-
-    pivot_index, swapping_index = _get_swapping_indexes()
+    executing_func = _get_swapping_indexes_TO if use_time_optimised else _get_swapping_indexes
+    pivot_index, swapping_index = executing_func()
 
     if pivot_index is None:
         return
@@ -66,11 +72,12 @@ def get_next_greater_num(num: []):
 
 # driver code
 def run():
-    num = '218765'
+    # num = '218765'
     num = '28765'
-    num = '321'
+    # num = '321'
     num_arr = list(map(int, num))
     next_greater_num = get_next_greater_num(num_arr)
+    # next_greater_num = get_next_greater_num(num_arr, use_time_optimised=True)
 
     if next_greater_num:
         print(f'Next Greater Number of {num} with same set of digits, is: {next_greater_num}')
