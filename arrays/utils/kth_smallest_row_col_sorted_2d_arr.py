4d11916341b8ea620c8ec0adf3bb77b5051e2132
veerat-beri <veerat@primedigital.tech>
2019-04-23 13:04:15 +0530

    wip

--- arrays/utils/kth_smallest_row_col_sorted_2d_arr.py
+++ arrays/utils/kth_smallest_row_col_sorted_2d_arr.py
@@ -7,31 +7,44 @@ from collections import namedtuple
 from heaps.heap import CustomNodeMinHeap
 
 
-def get_kth_smallest_in_sorted_2d_arr(arr: [], k: int, total_rows: int, total_cols: int):
-    assert arr, 'Array can not be empty'
-    assert k <= total_cols * total_rows, 'K should be in range of total array elements'
+class KthSmallestUsingCustomHeap:
+    def _validate_input(self, arr: [], k: int, total_rows: int, total_cols: int):
+        # Input Validations
+        assert arr, 'Array can not be empty'
+        assert k <= total_cols * total_rows, 'K should be in range of total array elements'
 
-    heap_node = namedtuple('heap_node', ['data', 'arr_row', 'arr_col'])
+    def __init__(self, arr: [], k: int, total_rows: int, total_cols: int):
+        self._validate_input(arr, k, total_rows, total_cols)
 
-    def _get_new_node(data: int, arr_row: int, arr_col: int):
-        return heap_node(data, arr_row, arr_col)
+        self.arr = arr
+        self.k = k
+        self.heap_node = namedtuple('heap_node', ['data', 'arr_row', 'arr_col'])
 
-    def _get_processed_row():
-        heap_arr = [_get_new_node(arr[0][col_index], 0, col_index) for col_index in range(len(arr[0]))]
+    def _get_new_node(self, data: int, arr_row: int, arr_col: int):
+        return self.heap_node(data, arr_row, arr_col)
+
+    def _get_processed_row(self):
+        heap_arr = [self._get_new_node(self.arr[0][col_index], 0, col_index) for col_index in range(len(self.arr[0]))]
         return heap_arr
 
-    heap = CustomNodeMinHeap(_get_processed_row())
+    def get_heap_inst(self):
+        return CustomNodeMinHeap(self._get_processed_row())
 
-    for _ in range(k - 1):
-        smallest_node = heap.pop()
-        try:
-            pushing_elem_row = smallest_node.arr_row + 1
-            pushing_elem_col = smallest_node.arr_col
-            heap.push(_get_new_node(arr[pushing_elem_row][pushing_elem_col], pushing_elem_row, pushing_elem_col))
-        except IndexError:
-            continue
+    def get_pushing_node(self, popped_node):
+        pushing_elem_row = popped_node.arr_row + 1
+        pushing_elem_col = popped_node.arr_col
+        return self._get_new_node(self.arr[pushing_elem_row][pushing_elem_col], pushing_elem_row, pushing_elem_col)
 
-    return heap.pop().data
+    def get_kth_smallest_in_sorted_2d_arr(self):
+        heap = self.get_heap_inst()
+
+        for _ in range(self.k - 1):
+            popped_node = heap.pop()
+            try:
+                heap.push(self.get_pushing_node(popped_node))
+            except IndexError:
+                continue
+        return heap.pop().data
 
 
 # driver code
@@ -43,12 +56,12 @@ def run():
         [32, 33, 39, 50, ],
     ]
     k = 7
-    kth_smallest_node = get_kth_smallest_in_sorted_2d_arr(arr, k, 4, 4)
+    kth_smallest_node = KthSmallestUsingCustomHeap(arr, k, 4, 4).get_kth_smallest_in_sorted_2d_arr()
 
-    if kth_smallest_node:
-        print(f'Kth({k}) smallest elem in given sorted 2D arr is: {kth_smallest_node}')
-    else:
-        print('No Kth({k}) smallest elem exists')
+    # if kth_smallest_node:
+    print(f'Kth({k}) smallest elem in given sorted 2D arr is: {kth_smallest_node}')
+    # else:
+    #     print('No Kth({k}) smallest elem exists')
 
 
 if __name__ == '__main__':
--- arrays/utils/merge_k_sorted_arrays.py
+++ arrays/utils/merge_k_sorted_arrays.py
@@ -1,23 +1,37 @@
 # Problem Statement
 # https://www.geeksforgeeks.org/merge-k-sorted-arrays/
 
+from arrays.utils.kth_smallest_row_col_sorted_2d_arr import KthSmallestUsingCustomHeap
 
-def get_merged_array(sorted_arrays: [[]], k: int, use_min_heap=True):
+
+class MergeKSortedArrays(KthSmallestUsingCustomHeap):
+    def __init__(self):
+    def get_pushing_node(self, popped_node):
+        pushing_elem_row = popped_node.arr_row
+        pushing_elem_col = popped_node.arr_col + 1
+        return self._get_new_node(self.arr[pushing_elem_row][pushing_elem_col], pushing_elem_row, pushing_elem_col)
+
     # Using MinBinaryHeap
+    def __get_merged_arr_using_heap(self):
+        return self.get_kth_smallest_in_sorted_2d_arr()
 
-    def _get_merged_arr_using_heap():
+    def __get_merged_arr_using_sort(self):
         pass
 
-    def _get_merged_arr_using_sort():
-        pass
-
-    executing_funct = _get_merged_arr_using_heap if use_min_heap else _get_merged_arr_using_sort
-    return executing_funct()
+    def get_merged_array(self, use_min_heap=True):
+        executing_func = self.__get_merged_arr_using_heap if use_min_heap else self.__get_merged_arr_using_sort
+        return executing_func()
 
 
 # driver code
 def run():
-    pass
+    arr = [
+        [1, 3, 5, 7, ],
+        [2, 4, 6, 8, ],
+        [0, 9, 10, 11, ],
+    ]
+    k = 3
+    MergeKSortedArrays(arr, k).get_merged_array()
 
 
 if __name__ == '__main__':
