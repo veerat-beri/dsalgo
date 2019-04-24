0bd295d9a27e03301195dd83171bb6a35d021a14
veerat-beri <veerat@primedigital.tech>
2019-04-24 11:03:01 +0530

    wip

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,15 +1,30 @@
 # Problem Statement
 # https://www.geeksforgeeks.org/merge-sort/
 
+from arrays.utils.merge_k_sorted_arrays import MergeKSortedArrays
 
-def get_sorted_arr(arr: []):
-    def _get_sorted_arr(low, high, ):
-        if low > high:
+
+class MergeSort:
+    def __init__(self, arr: []):
+        self.output_arr = []
+        self.input_arr = arr
+
+    def get_sorted_merged_arrays(self, low: int, mid: int, high: int):
+        arr1_start_index = low
+        arr_2_start_index = high - mid + 1
+        # while arr1_index <= mid_index and
+
+
+    def get_sorted_arr(self, arr: [], low, high):
+        # def _get_sorted_arr(low, high, ):
+        if low >= high:
             return
 
         mid = (high - low)//2 + low
-        _get_sorted_arr(low, mid)
-        _get_sorted_arr(mid + 1, high)
+        self.get_sorted_arr(arr, low, mid)
+        self.get_sorted_arr(arr, mid + 1, high)
+
+
 
 
 
