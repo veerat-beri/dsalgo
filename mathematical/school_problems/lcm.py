9e2181c941ee8a6659b0c4ffed2d2830775a9915
veerat-beri <veerat@primedigital.tech>
2019-05-06 21:24:51 +0530

    wip

---
+++ mathematical/school_problems/lcm.py
@@ -0,0 +1,13 @@
+# Problem Statement
+# https://www.geeksforgeeks.org/program-to-find-lcm-of-two-numbers/
+
+def get_lcm():
+    pass
+
+# driver code
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
--- strings/utils/power_sets.py
+++ strings/utils/power_sets.py
@@ -1,10 +1,25 @@
 # Problem Statement
 # https://www.geeksforgeeks.org/power-set/
+# https://www.geeksforgeeks.org/recursive-program-to-generate-power-set/
+
+
+def get_power_set(string: str):
+    # Recursive Approach
+    str_len = len(string)
+
+    def _get_power_set(str_index: int = 0, str_so_far: str = ''):
+        print(str_so_far, str_index)
+        if str_index < str_len:
+            _get_power_set(str_index + 1, str_so_far + string[str_index])
+            _get_power_set(str_index + 1, str_so_far)
+
+    _get_power_set()
 
 
 # driver code
 def run():
-    pass
+    string = 'abc'
+    get_power_set(string)
 
 
 if __name__ == '__main__':
