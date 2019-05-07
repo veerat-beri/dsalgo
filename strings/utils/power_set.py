a0e9527549fc7169fe66ed2ee329a02ccfce0001
veerat-beri <veerat@primedigital.tech>
2019-05-07 22:35:44 +0530

    wip

--- strings/utils/power_set.py
+++ strings/utils/power_set.py
@@ -5,7 +5,9 @@
 def get_power_set(string: str):
     str_len = len(string)
 
-    # Recursive Approach
+    ############################################################
+    # Recursive Approach-1
+    ############################################################
     def _get_power_set(str_index: int = 0, str_so_far: str = ''):
         if str_index == str_len:
             yield str_so_far
@@ -16,17 +18,38 @@ def get_power_set(string: str):
             for sub_str in _get_power_set(str_index + 1, str_so_far):
                 yield sub_str
 
-    # different Recursive Approach
-    def _get_power_set_appr_2(str_index: int = 0, str_so_far: str = ''):
+    def _print_power_set(str_index: int = 0, str_so_far: str = ''):
         if str_index == str_len:
             print(str_so_far)
-            return
-        for successive_str_index in range(str_index + 1, str_len):
-            _get_power_set_appr_2(successive_str_index, str_so_far + string[successive_str_index])
 
+        if str_index < str_len:
+            _print_power_set(str_index + 1, str_so_far + string[str_index])
+            _print_power_set(str_index + 1, str_so_far)
 
     # return _get_power_set()
-    _get_power_set_appr_2()
+    # _print_power_set()
+
+    ############################################################
+    # Recursive Approach-2
+    ############################################################
+    def _get_power_set_approach_2(str_index: int = -1, str_so_far: str = ''):
+        yield str_so_far
+        for successive_str_index in range(str_index + 1, str_len):
+            for sub_str in _get_power_set_approach_2(successive_str_index, str_so_far + string[successive_str_index]):
+                yield sub_str
+
+    def _print_power_set_approach_2(str_index: int = -1, str_so_far: str = ''):
+        print(str_so_far)
+        for successive_str_index in range(str_index + 1, str_len):
+            _print_power_set_approach_2(successive_str_index, str_so_far + string[successive_str_index])
+
+    # return _get_power_set_approach_2()
+    _print_power_set_approach_2()
+
+    ############################################################
+    # Iterative Approach
+    ############################################################
+    # Its covered under bit-manipulation app
 
 
 # driver code
