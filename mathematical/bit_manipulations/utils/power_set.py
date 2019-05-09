62386d9880d3be3b4c9d8c113861753de821c88c
veerat-beri <veerat@primedigital.tech>
2019-05-09 10:56:40 +0530

    wip

--- mathematical/bit_manipulations/utils/power_set.py
+++ mathematical/bit_manipulations/utils/power_set.py
@@ -3,17 +3,33 @@
 
 from typing import Set
 
+from mathematical.bit_manipulations.utils.get_set_bits_pos import get_set_bits_pos
+
 
 def get_power_set(cet: Set):
     set_len = len(cet)
-    for num in range(set_len):
-        # if num >>
-        pass
+    list_from_set = list(cet)
+    for num in range(2**set_len):
+        c_num = num
+        power_set_elem = ''
+
+
+        print('num: ', num)
+        for set_bit_pos in get_set_bits_pos(c_num):
+
+
+            print('set_bit_pos: ', set_bit_pos)
+            power_set_elem += list_from_set[set_bit_pos - 1]
+
+        yield power_set_elem
 
 
 # driver code
 def run():
-    pass
+    cet = {'a', 'b', 'c', }
+    for power_set_elem in get_power_set(cet):
+        # print(power_set_elem)
+        pass
 
 
 if __name__ == '__main__':
