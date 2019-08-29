66fc0c43cb50a19146dca7fa7506b08a902ebd42
Veerat <viratberi@gmail.com>
2019-08-29 11:23:06 +0530

    wip

--- strings/utils/anagrams_substring_search.py
+++ strings/utils/anagrams_substring_search.py
@@ -8,51 +8,67 @@ from copy import copy
 # This Approach did not work, due to ambiguity in judging whether
 # to consider chars from previous window(in case pattern chars are matching successively)
 # or not consider them (when previous window has chars which do not belong to pattern string)
-class AnagramSubstringSearch:
-    def __init__(self, pattern_str: str, text_str: str):
+# class AnagramSubstringSearch:
+#     def __init__(self, pattern_str: str, text_str: str):
+#         self.pattern_str = pattern_str
+#         self.text_str = text_str
+#         self.pattern_str_len = len(pattern_str)
+#         self.patter_str_chars_count = defaultdict(int)
+#         self.required_chars_count = dict()
+#         self._init_patter_str_chars_count()
+#         self._reset_required_chars_count()
+#
+#     def _increase_required_char_count(self, str_index):
+#         required_str_index = str_index - self.pattern_str_len
+#         if required_str_index >= 0:
+#             char = self.text_str[required_str_index]
+#
+#             # If that char is part of pattern string
+#             if self.patter_str_chars_count.get(char):
+#                 self.required_chars_count[char] += 1
+#
+#     def _reduce_required_char_count(self, str_index):
+#         char = self.text_str[str_index]
+#         self.required_chars_count[char] -= 1
+#
+#         if not self.required_chars_count[char]:
+#             del self.required_chars_count[char]
+#
+#     def _reset_required_chars_count(self):
+#         self.required_chars_count = copy(self.patter_str_chars_count)
+#
+#     def _init_patter_str_chars_count(self):
+#         for char in self.pattern_str:
+#             self.patter_str_chars_count[char] += 1
+#
+#     def get_all_anagrams_pos(self):
+#         for str_index in range(len(self.text_str)):
+#             if self.text_str[str_index] not in self.patter_str_chars_count:
+#                 self._reset_required_chars_count()
+#             else:
+#                 self._increase_required_char_count(str_index)
+#                 self._reduce_required_char_count(str_index)
+#
+#             if not self.required_chars_count:
+#                 print(str_index - self.pattern_str_len + 1)
+#                 # self._increase_required_char_count(str_index)
+from strings.utils.string_matching.rabin_karp import RabinKarpStringMatch
+
+
+class AnagramsSearch(RabinKarpStringMatch):
+    def __init__(self, pattern_str, text_str, ):
         self.pattern_str = pattern_str
         self.text_str = text_str
         self.pattern_str_len = len(pattern_str)
         self.patter_str_chars_count = defaultdict(int)
         self.required_chars_count = dict()
-        self._init_patter_str_chars_count()
-        self._reset_required_chars_count()
 
-    def _increase_required_char_count(self, str_index):
-        required_str_index = str_index - self.pattern_str_len
-        if required_str_index >= 0:
-            char = self.text_str[required_str_index]
+    def pattern_matches(self):
+        # No need to check for exact pattern match any combination of pattern chars would work
+        return True
 
-            # If that char is part of pattern string
-            if self.patter_str_chars_count.get(char):
-                self.required_chars_count[char] += 1
-
-    def _reduce_required_char_count(self, str_index):
-        char = self.text_str[str_index]
-        self.required_chars_count[char] -= 1
-
-        if not self.required_chars_count[char]:
-            del self.required_chars_count[char]
-
-    def _reset_required_chars_count(self):
-        self.required_chars_count = copy(self.patter_str_chars_count)
-
-    def _init_patter_str_chars_count(self):
-        for char in self.pattern_str:
-            self.patter_str_chars_count[char] += 1
-
-    def get_all_anagrams_pos(self):
-        for str_index in range(len(self.text_str)):
-            if self.text_str[str_index] not in self.patter_str_chars_count:
-                self._reset_required_chars_count()
-            else:
-                self._increase_required_char_count(str_index)
-                self._reduce_required_char_count(str_index)
-
-            if not self.required_chars_count:
-                print(str_index - self.pattern_str_len + 1)
-                # self._increase_required_char_count(str_index)
-
+    def get_all_permuted_pattern_indexes(self):
+        RabinKarpStringMatch()
 
 # driver code
 def run():
--- strings/utils/string_matching/rabin_karp.py
+++ strings/utils/string_matching/rabin_karp.py
@@ -31,33 +31,39 @@ class RabinKarpStringMatch:
 
         for str_index in range(self.pattern_str_len):
             pattern_hash = (pattern_hash * self.NO_OF_CHARS + ord(self.pattern_str[str_index])) % self.PRIME_NUM
-            text_hash_so_far = (text_hash_so_far * self.NO_OF_CHARS + ord(self.text_str[str_index])) % self.PRIME_NUM
+            text_hash_so_far = (self.NO_OF_CHARS * text_hash_so_far + ord(self.text_str[str_index])) % self.PRIME_NUM
 
         return pattern_hash, text_hash_so_far
 
-    def check_text_window(self, text_window_start_index):
+    def pattern_matches(self, text_window_start_index):
         for str_index in range(self.pattern_str_len):
             if self.text_str[text_window_start_index + str_index] != self.pattern_str[str_index]:
                 return False
         return True
 
-    def get_pattern_occurrences(self):
-        pattern_occurrences_indexes = []
+    def __get_updated_text_window_hash(self, str_index, text_hash_so_far):
+        return (self.NO_OF_CHARS * (text_hash_so_far - ord(self.text_str[str_index]) * self.h) + ord(self.text_str[str_index + self.pattern_str_len])) % self.PRIME_NUM
+
+    def get_pattern_occurrence_indexes(self):
+        max_pos_for_hash_computations = self.text_str_len - self.pattern_str_len
         pattern_hash, text_hash_so_far = self.get_hash_values()
-        for str_index in range(self.text_str_len - self.pattern_str_len + 1):
-            if pattern_hash == text_hash_so_far:
-                if self.check_text_window(str_index):
-                    pattern_occurrences_indexes.append(str_index)
-
-
-
-
-
-
+        for str_index in range(max_pos_for_hash_computations + 1):
+            if pattern_hash == text_hash_so_far and self.pattern_matches(str_index):
+                yield str_index
+            if str_index < max_pos_for_hash_computations:
+                self.__get_updated_text_window_hash(str_index, text_hash_so_far)
 
 # driver code
 def run():
-    pass
+    text_str = 'AABAACAADAABAABA'
+    pattern_str = 'AABA'
+
+    print('\nText String: \n{}'.format(text_str))
+    print(f'Pattern String: {pattern_str}')
+    print('Pattern found at indexes: ')
+    for pattern_occurrence_index in RabinKarpStringMatch(pattern_str, text_str).get_pattern_occurrence_indexes():
+        # print(text_str[pattern_occurrence_index: pattern_occurrence_index + len(pattern_str)])
+        print(pattern_occurrence_index)
 
 
 if __name__ == '__main__':
