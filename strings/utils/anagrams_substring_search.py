f61f8b119c18f59edb8db038da4de108c8d7c5aa
Veerat <viratberi@gmail.com>
2019-09-06 22:34:04 +0530

    wip

--- strings/utils/anagrams_substring_search.py
+++ strings/utils/anagrams_substring_search.py
@@ -5,6 +5,7 @@
 # to consider chars from previous window(in case pattern chars are matching successively)
 # or not consider them (when previous window has chars which do not belong to pattern string)
 from collections import defaultdict
+from copy import copy
 
 
 class AnagramSubstringSearch:
@@ -14,12 +15,18 @@ class AnagramSubstringSearch:
         self.pattern_str_len = len(pattern_str)
         self.text_str_len = len(text_str)
         self.text_window_extra_chars_count = dict()
-        self._reset_required_chars_count()
+        self.__reset_text_window_extra_chars_count()
         self.__pattern_char_count_dict = None
+        self.__whitelisted_str_index = None
 
-    @property
-    def remove_char(self):
-        return
+    def should_remove_char(self, present_str_index):
+        if self.__whitelisted_str_index and self.__whitelisted_str_index < present_str_index:
+            self.__whitelisted_str_index = None
+            return False
+        return True
+
+    def __set_white_listed_str_index(self, str_index):
+        self.__whitelisted_str_index = str_index
 
     @property
     def pattern_char_count_dict(self):
@@ -27,23 +34,23 @@ class AnagramSubstringSearch:
             self.__pattern_char_count_dict = self.__get_patter_str_chars_count()
         return self.__pattern_char_count_dict
 
-    def _remove_last_window_char(self, str_index):
-        required_str_index = str_index - self.pattern_str_len
-        if required_str_index >= 0:
-            char = self.text_str[required_str_index]
+    def __remove_last_window_char(self, str_index):
+        char_index_to_be_removed = str_index - self.pattern_str_len
+        if char_index_to_be_removed >= 0 and self.should_remove_char(char_index_to_be_removed):
+            char = self.text_str[char_index_to_be_removed]
 
             # # If that char is part of pattern string
             # if self.pattern_char_count_dict.get(char):
             self.text_window_extra_chars_count[char] += 1
 
-    def _add_new_window_char(self, str_index):
+    def __add_new_window_char(self, str_index):
         char = self.text_str[str_index]
         self.text_window_extra_chars_count[char] -= 1
 
         if not self.text_window_extra_chars_count[char]:
             del self.text_window_extra_chars_count[char]
 
-    def _reset_required_chars_count(self):
+    def __reset_text_window_extra_chars_count(self):
         self.text_window_extra_chars_count = copy(self.pattern_char_count_dict)
 
     def __get_patter_str_chars_count(self):
@@ -57,16 +64,15 @@ class AnagramSubstringSearch:
         str_index = 0
         while str_index < self.text_str_len:
             if self.text_str[str_index] not in self.pattern_char_count_dict:
-                self._reset_required_chars_count()
-
+                self.__reset_text_window_extra_chars_count()
+                self.__set_white_listed_str_index(str_index)
             else:
-                # if
-                self._remove_last_window_char(str_index)
-                self._add_new_window_char(str_index)
+                self.__remove_last_window_char(str_index)
+                self.__add_new_window_char(str_index)
 
             if not self.text_window_extra_chars_count:
                 print(str_index - self.pattern_str_len + 1)
-                # self._remove_last_window_char(str_index)
+                # self.__remove_last_window_char(str_index)
 # from strings.utils.string_matching.rabin_karp import RabinKarpStringMatch
 #
 #
