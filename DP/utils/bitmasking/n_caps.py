33f57a7af70c3a964be41ced287afdb64af715e3
Veerat <viratberi@gmail.com>
2019-10-12 15:42:31 +0530

    fixed heap check logic

--- DP/utils/bitmasking/n_caps.py
+++ DP/utils/bitmasking/n_caps.py
@@ -1,9 +1,26 @@
 # Problem Statement
 # https://www.geeksforgeeks.org/bitmasking-and-dynamic-programming-set-1-count-ways-to-assign-unique-cap-to-every-person/
 
+from typing import List
 
-def get_total_arrangements():
-    pass
+
+def get_total_arrangements(caps_collections: List[List[int]]):
+    total_cap_collections = len(caps_collections)
+
+    def _process_possible_arrangements(selected_caps_set, collection_index, possible_arrangements_count):
+        if collection_index >= total_cap_collections:
+            return possible_arrangements_count + 1
+
+        caps_collection = caps_collections[collection_index]
+        for cap in caps_collection:
+            if cap in selected_caps_set:
+                continue
+            selected_caps_set.add(cap)
+            possible_arrangements_count = _process_possible_arrangements(selected_caps_set, collection_index + 1, possible_arrangements_count)
+            selected_caps_set.remove(cap)
+        return possible_arrangements_count
+
+    return _process_possible_arrangements(set(), 0, 0)
 
 
 # driver code
--- arrays/utils/check_array_repr_heap.py
+++ arrays/utils/check_array_repr_heap.py
@@ -1,40 +1,103 @@
 # Problem Statement
 # https://www.geeksforgeeks.org/how-to-check-if-a-given-array-represents-a-binary-heap/
 
+from heaps.heap import MaxBinaryHeap, MinBinaryHeap
 
-def do_arr_repr_heap(heap: [], heap_type='MAX'):
-    heap_len = len(heap)
-    index = 0
-    if heap_type == 'MAX':
-        for index in range(heap_len - heap_len//2):
-            try:
-                if heap[index] >= heap[2*index + 1] and heap[index] >= heap[2*index + 2]:
-                    continue
-            except IndexError:
-                pass
-            break
-    else:
-        for index in range(heap_len - heap_len//2):
-            try:
-                if heap[index] <= heap[2*index + 1] and heap[index] <= heap[2*index + 2]:
-                    continue
-            except IndexError:
-                pass
-            break
+# Fails for heap = [65, 91, ].
+# As loop break could occur due to multiple reasons and we can not judge
+# end "index" value is due to loop termination or break, in case arr is small as [65, 91, ]
 
-    if index >= heap_len - heap_len//2 - 1:
+# def do_arr_repr_heap(heap: [], heap_type='MAX'):
+#     heap_len = len(heap)
+#     index = 0
+#     if heap_type == 'MAX':
+#         for index in range(heap_len - heap_len//2):
+#             try:
+#                 if heap[index] >= heap[2*index + 1] and heap[index] >= heap[2*index + 2]:
+#                     continue
+#             except IndexError:
+#                 pass
+#             break
+#     else:
+#         for index in range(heap_len - heap_len//2):
+#             try:
+#                 if heap[index] <= heap[2*index + 1] and heap[index] <= heap[2*index + 2]:
+#                     continue
+#             except IndexError:
+#                 pass
+#             break
+#
+#     if index >= heap_len - heap_len//2 - 1:
+#         return True
+#     return False
+
+
+class CheckArrIsHeap:
+    MAX = MaxBinaryHeap
+    MIN = MinBinaryHeap
+
+    ITERATIVE = 'iterative'
+    RECURSIVE = 'recursive'
+
+    def __init__(self, arr: [], heap_class=MAX, exec_method=ITERATIVE):
+        self.heap = arr
+        self.heap_len = len(arr)
+        self.heap_class = heap_class
+        self.exec_method = exec_method
+
+    def __get_children(self, parent_index: int):
+        left_child_index = self.heap_class.get_left_child_index(parent_index)
+        right_child_index = self.heap_class.get_right_child_index(parent_index)
+
+        left_child = self.heap[left_child_index]
+        right_child = self.heap[right_child_index] if right_child_index < self.heap_len else None
+
+        return left_child, right_child
+
+    def __is_not_heap_node(self, parent, left_child, right_child):
+        if self.heap_class == self.MAX:
+            return parent < left_child or (right_child and parent < right_child)
+        return parent > left_child or (right_child and parent > right_child)
+
+    # Iterative Approach
+    def __iterative(self, arr_index, last_internal_node):
+        for index in range(arr_index, last_internal_node):
+            parent = self.heap[index]
+            left_child, right_child = self.__get_children(index)
+
+            if self.__is_not_heap_node(parent, left_child, right_child):
+                return False
+
         return True
-    return False
+
+    # Recursive Approach
+    def __recursive(self, arr_index, last_internal_node):
+        if arr_index >= last_internal_node:
+            return True
+
+        parent = self.heap[arr_index]
+        left_child, right_child = self.__get_children(arr_index)
+
+        if self.__is_not_heap_node(parent, left_child, right_child):
+            return False
+
+        return self.__recursive(arr_index + 1, last_internal_node)
+
+    def do_arr_repr_heap(self):
+        exec_func = self.__iterative if self.exec_method == self.ITERATIVE else self.__recursive
+        return exec_func(0, self.heap_len // 2)
 
 
 # driver code
 def run():
     heap = [90, 15, 10, 7, 12, 2]
+    # heap = [65, 91, ]
     print(f'Given arr: {heap}')
-    if do_arr_repr_heap(heap):
-        print('Represents Max Heap')
+
+    if CheckArrIsHeap(heap, CheckArrIsHeap.MAX, CheckArrIsHeap.RECURSIVE).do_arr_repr_heap():
+        print('Represents Heap')
     else:
-        print('It does not represent Max Heap')
+        print('It does not represent Heap')
 
 
 if __name__ == '__main__':
